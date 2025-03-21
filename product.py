88f9e097cdb8b54f6927d718c08a5f716beeb8ea
Pradip Sapkota <pradipsapkota676@gmail.com>
2025-03-21 07:36:42 -0500

    clear instructions in product.py

--- product.py
+++ product.py
@@ -1,6 +1,6 @@
 def main():
     print("I am a software made by 11850393")
-    #print("Malicious code!")   #For the interception part
+    #print("Malicious code!")   #For the interception part(Uncomment this to intercept)
 
 if __name__ == "__main__":
     main()
